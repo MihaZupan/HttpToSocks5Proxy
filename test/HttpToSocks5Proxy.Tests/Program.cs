230553299cf379031f091efbeae9c76f68521868
Miha Zupan <mihazupan.zupan1@gmail.com>
2019-04-06 09:41:36 +0200

    Bump version to 1.1.3

--- test/HttpToSocks5Proxy.Tests/Program.cs
+++ test/HttpToSocks5Proxy.Tests/Program.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using MihaZupan;
 
 namespace Socks5Proxy.Tests
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
             var proxy = new HttpToSocks5Proxy(new[] {
                 new ProxyInfo("proxy-server.com", 1080),
             });
             var handler = new HttpClientHandler { Proxy = proxy };
             HttpClient httpClient = new HttpClient(handler, true);
-            var httpsGet = httpClient.SendAsync(
+
+            var result = await httpClient.SendAsync(
                 new HttpRequestMessage(HttpMethod.Get, "https://httpbin.org/ip"));
-            var result = httpsGet.Result;
+
             result.EnsureSuccessStatusCode();
 
-            Console.WriteLine("HTTPS GET: " + result.Content.ReadAsStringAsync().Result);
+            Console.WriteLine("HTTPS GET: " + await result.Content.ReadAsStringAsync());
 
             Console.ReadLine();
         }
